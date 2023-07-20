adcc035845e537747107803b975b01e729db4d75
Th3Fr3ak <40581656+LOLatom@users.noreply.github.com>
2023-07-20 19:56:29 +0200

    Start of Specialised Crafting -Added Specialised Crafting table Block

--- src/main/java/com/thefreak/botsmod/objects/containers/SpecialisedCraftingMenu.java
+++ src/main/java/com/thefreak/botsmod/objects/containers/SpecialisedCraftingMenu.java
@@ -2,6 +2,7 @@ package com.thefreak.botsmod.objects.containers;
 
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.MenuType;
 import org.jetbrains.annotations.Nullable;
 
@@ -9,8 +10,10 @@ public class SpecialisedCraftingMenu extends AbstractContainerMenu {
 
     private static final int INV_SLOT_START = 10;
     private static final int INV_SLOT_END = 37;
-    protected SpecialisedCraftingMenu(@Nullable MenuType<?> pMenuType, int pContainerId) {
+    private final ContainerLevelAccess access;
+    protected SpecialisedCraftingMenu(@Nullable MenuType<?> pMenuType, int pContainerId, ContainerLevelAccess access) {
         super(pMenuType, pContainerId);
+        this.access = access;
     }
 
     @Override
